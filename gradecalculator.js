375500ef92a84d4663e80866e617f87b81d7088b
phildierks <phillipdierks@students.berkeley.net>
2017-12-04 22:27:04 -0800

    Homework Almost Done

--- gradecalculator.js
+++ gradecalculator.js
@@ -5,26 +5,54 @@ function calculateCurrentGrade(){
     var midterm = averageArray(document.getElementById("midterm").value);
     var gradeArray = [];
     gradeArray.push(homework,quiz,test,midterm);
+    console.log(gradeArray);
     var weight = weights();
     var currentGrade = 0;
     for(var i = 0; i < gradeArray.length;i++){
         currentGrade += (gradeArray[i]*weight[i]);
     }
+    var weightSum = weight[0]+weight[1]+weight[2]+weight[3];
+    if(weightSum != 1){
+        alert("Your Weights Must Add up to 100");
+        return;
+    }
+    currentGrade = currentGrade/weightSum;
     currentGrade = Math.round(currentGrade*100);
+    if(isNaN(currentGrade)){
+        alert("You made an Error when entering your Grades");
+        document.getElementById("currentGrade").innerHTML = "";
+        return;
+    }
     document.getElementById("currentGrade").innerHTML = "Your Current Grade: " + currentGrade + "%";
     colorCode(document.getElementById("hw"),homework);
+    colorCode(document.getElementById("quizzes"),quiz);
+    colorCode(document.getElementById("tests"),test);
+    colorCode(document.getElementById("mid"),midterm);
     return currentGrade;
 }
 
+
+
 function calculateFinalGrade(){
-    document.getElementById("finalNeeded").innerHTML = "You Need: ";
-    var finalWeight = (document.getElementById("finalWeight").value);
-    var wantedGrade = (document.getElementById("wantedGrade").value);
+    document.getElementById("finalNeeded").innerHTML = "";
+    document.getElementById("wrongInputs").innerHTML = "";
+    var finalWeight = document.getElementById("finalWeight").value;
+    var wantedGrade = document.getElementById("wantedGrade").value;
     var currentGrade = calculateCurrentGrade();
+    if(isNaN(wantedGrade)){
+        alert("You made an Error when entering your wanted grade");
+        document.getElementById("wantedGrade").value = "";
+        return;
+    }
+    if(isNaN(finalWeight)){
+       alert("You made an Error when entering your final weight");
+       document.getElementById("finalWeight").value = "";
+        return;
+    }
     console.log(currentGrade);
-    var finalGrade = (wantedGrade - currentGrade)/finalWeight;
+    var finalGrade = ((wantedGrade*100)-currentGrade*(100-finalWeight))/finalWeight;
     console.log(finalGrade);
-    finalGrade = Math.round(finalGrade*100);
+    finalGrade = Math.round(finalGrade);
     console.log(finalGrade);
     document.getElementById("finalNeeded").innerHTML = "You Need a " + finalGrade + "% on the final!";
 }
@@ -43,6 +71,10 @@ function averageArray(arr){
     return sum/arr.length/100;
 }
 
+
+
+
+
 function weights(){
     var hwweight = parseInt(document.getElementById("homeworkWeight").value)/100;
     var quizweight = parseInt(document.getElementById("quizWeight").value)/100;
@@ -53,14 +85,25 @@ function weights(){
         console.log(weightArray);
         return weightArray;
     }
+
+
+
 function colorCode(element,avg){
-
     if(avg > .89){
-        element.style.background = "green";
+        element.style.background = "#91ff35";
     }
-
-
-
+    if(avg > .79 && avg <= .89){
+        element.style.background = "#ffa6fa";
+    }
+    if(avg > .69 && avg <= .79){
+        element.style.background = "yellow";
+    }
+    if(avg > .59 && avg <= .69){
+        element.style.background = "orange";
+    }
+    if(avg <= .59){
+        element.style.background = "red";
+    }
 }
 
 
